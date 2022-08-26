b6dae7901302113083e191ab8de8c132e3073997
Shundrik <102665832+Shundrik@users.noreply.github.com>
2022-08-26 22:17:49 +0300

    hw-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+ 
+const galleryRef = document.querySelector(".gallery");
+// galleryRef.addEventListener("click",)
+ 
+function createImage(galleryItems){
+    return galleryItems.map((e)=>{
+        // console.log(e.preview);
+        return `<a
+        class="gallery__link"
+        href="${e.original}">
+        <img
+        class="gallery__image"
+        srs="${e.preview}"
+        data-source="${e.original}"
+        alt="${e.description}"
+        loading="lazy"
+        ></img>
+        </a>`;
+        }).join(" ");
+    }
+ 
+galleryRef.insertAdjacentHTML('afterbegin',createImage(galleryItems));
